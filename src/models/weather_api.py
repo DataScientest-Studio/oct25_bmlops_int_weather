90baa02664b0b2bd057ef88ecae95c2be6544592
Xingran <xingran_wang@icloud.com>
2025-11-20 17:03:43 +0000

    add descriptions for return status

--- src/models/weather_api.py
+++ src/models/weather_api.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
-
-
 from fastapi import FastAPI
+
+
+responses = {
+    200: {"description": "OK"},
+    404: {"description": "Item not found"},
+    302: {"description": "The item was moved"},
+    403: {"description": "Not enough privileges"},
+}
+
 api = FastAPI(
     title='weather forcasting api'
 )
@@ -10,7 +17,7 @@ api = FastAPI(
 def get_index():
     return {'greeting': 'Welcome to weather forcasting api!'}
 
-@api.get('/predict')
+@api.get('/predict', responses=responses)
 def get_predict():
     try:
         # TODO: call the prediction model routine
@@ -18,7 +25,7 @@ def get_predict():
     except Exception as e:
         return {'error': str(e)}
 
-@api.get('/training')
+@api.get('/training', responses=responses)
 def get_training():
     try:
         # TODO: call the traning model routine
