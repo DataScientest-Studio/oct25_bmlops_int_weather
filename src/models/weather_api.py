0dfaad7b5fba99cc75661e9ec3a72e4bafa7bbbd
Xingran <xingran_wang@icloud.com>
2025-11-20 17:29:35 +0000

    enable the predicting api to fetch the weather of a specific day

--- src/models/weather_api.py
+++ src/models/weather_api.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from typing import Optional
 
 
 responses = {
@@ -22,12 +23,41 @@ def get_index():
     return {'greeting': 'Welcome to weather forcasting api!'}
 
 @api.get('/predict', name='Predict The Weather', responses=responses)
-def get_predict():
+def get_predict(day: Optional[int]=None):
+    # TODO: call the prediction model routine
     try:
         # TODO: call the prediction model routine
-        return {'predict': 'sunny'}
+        # predict the weather of following 7 days
+        weather = ['sunny', 'cloudy', 'rainy', 'snowy', 'windy', 'stormy', 'hazy']
     except Exception as e:
         return {'error': str(e)}
+
+    if day:
+        # how many days in the future
+        try:
+            return {'day': day,
+                    'predict': weather[day]}
+        except IndexError:
+            raise HTTPException(
+                status_code=404,
+                detail='Unknown Index')
+        except ValueError:
+            raise HTTPException(
+                status_code=400,
+                detail='Bad Type')
+    else:
+        # predict the weather of today
+        try:
+            return {'day': 0,
+                    'predict': weather[0]}
+        except IndexError:
+            raise HTTPException(
+                status_code=404,
+                detail='Unknown Index')
+        except ValueError:
+            raise HTTPException(
+                status_code=400,
+                detail='Bad Type')
 
 @api.get('/training', name='Train The Model with existing data', responses=responses)
 def get_training():
