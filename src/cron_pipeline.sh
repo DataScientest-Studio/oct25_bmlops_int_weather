be29d36a1c71020b3a280ef1c78adfcfdc10b2a5
Ubuntu <ubuntu@ip-172-31-42-125.eu-west-1.compute.internal>
2026-01-04 20:57:12 +0000

    deleting unnecessary fiess, commenting code and Streamlit outlook

--- src/cron_pipeline.sh
+++ src/cron_pipeline.sh
@@ -5,7 +5,7 @@ sleep 30
 
 echo "Start cron $(date)" >> "$LOG"
 
-
+# Trigger the dataset creation, preprocessing, and training via API
 curl -s http://localhost:8000/make_dataset >> "$LOG"
 curl -s http://localhost::8000/preprocessing >> "$LOG"
 curl -s http://localhost::8000/training >> "$LOG"
