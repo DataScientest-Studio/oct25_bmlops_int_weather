2946ef3295293cf0462c5cdd15b42d0dc129e18a
xingran <xingran_wang@icloud.com>
2026-01-11 17:10:09 +0100

    fix(cron_pipeline): add logging for curl commands in cron job

--- src/cron_pipeline.sh
+++ src/cron_pipeline.sh
@@ -6,8 +6,11 @@ sleep 30
 echo "Start cron $(date)" >> "$LOG"
 
 # Trigger the dataset creation, preprocessing, and training via API
+echo "curl -s ${MODEL_URI}/make_dataset" >> "$LOG"
 curl -s ${MODEL_URI}/make_dataset >> "$LOG"
+echo "curl -s ${MODEL_URI}/preprocessing" >> "$LOG"
 curl -s ${MODEL_URI}/preprocessing >> "$LOG"
+echo "curl -s ${MODEL_URI}/training" >> "$LOG"
 curl -s ${MODEL_URI}/training >> "$LOG"
 
 echo "End cron $(date)" >> "$LOG"
